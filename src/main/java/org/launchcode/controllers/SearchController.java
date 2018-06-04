db973b05c3b4071219c1e54b46be7577a2ef5607
Jonathan Ellis <joellis13@yahoo.com>
2018-06-04 15:22:52 -0500

    Almost done, just missing Results=0 and bonus missions.

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,35 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> jobsList = new ArrayList<>();
+        model.addAttribute("columns", ListController.columnChoices);
+
+        if (searchType.equals("all")) {
+            jobsList = JobData.findByValue(searchTerm);
+            model.addAttribute("jobs", jobsList);
+            return "search";
+        } else {
+            jobsList = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("jobs", jobsList);
+            return "search";
+        }
+
+    }
+
+    /*
+
+        model.addAttribute("jobsList", jobsList);
+        if (searchType.equals("All")) {
+            jobsList = JobData.findByValue(searchTerm);
+            return "search";
+        } else {
+            jobsList = JobData.findByColumnAndValue(searchType, searchTerm);
+            return "redirect:search/results";
+        }
+    }*/
 
 }
